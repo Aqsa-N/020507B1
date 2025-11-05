6b99f7295457a1d7ca4a588904a92706b91e96f3
faiz ali <faizalioffical355@gmail.com>
2025-11-05 10:55:00 +0500

    uploaded

---
+++ js/dom.js
@@ -0,0 +1,8 @@
+console.log(window.document);
+// var hh=document.getElementsBy("h");
+//var selector=document.querySelectorAll(".h");
+var element= document.querySelector('#demo');
+//console.log(element);
+element.innerHTML="<h1>DOM MANIPUlATION PARH chuky HAIn</h1>";
+element.style.color="white";
+element.style.backgroundColor="pink";

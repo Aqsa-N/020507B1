b8c9966cc4b8d3e00a889a371c51c5b357798660
faiz ali <faizalioffical355@gmail.com>
2025-11-07 10:56:17 +0500

    saved

--- js/dom.js
+++ js/dom.js
@@ -3,6 +3,20 @@ console.log(window.document);
 //var selector=document.querySelectorAll(".h");
 var element= document.querySelector('#demo');
 //console.log(element);
-element.innerHTML="<h1>DOM MANIPUlATION PARH chuky HAIn</h1>";
-element.style.color="white";
-element.style.backgroundColor="pink";
+// element.innerHTML="<h1>DOM MANIPUlATION PARH chuky HAIn</h1>";
+// element.style.color="white";
+// element.style.backgroundColor="pink";
+// element.addEventListener("click",function(){
+//     element.innerHTML="<h1>DOM MANIPUlATION PARH chuky HAIn</h1>";
+//     element.style.color="white";
+//     element.style.backgroundColor="pink";
+// });
+var para=document.querySelectorAll('p');
+//console.log(para);
+para.forEach(function (i){
+    //console.log(i);
+    i.style.color="orange";
+}
+)
+
+
